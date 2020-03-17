c2cad5b90adead346410705c34742af34dd51393
Andrey <Elektron313>
2020-03-17 14:50:44 +0300

    Updates

---
+++ precache-manifest.38b8e7270e32693e3ddc7de193dce184.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbecac4848325f56d1ad62d659ed1f35",
+    "url": "/Elektron313/social-network/index.html"
+  },
+  {
+    "revision": "e96da2ccb0da048072af",
+    "url": "/Elektron313/social-network/static/css/3.ee388f0e.chunk.css"
+  },
+  {
+    "revision": "98e7c67ab7d8c3a25725",
+    "url": "/Elektron313/social-network/static/css/4.56fe1bf8.chunk.css"
+  },
+  {
+    "revision": "f3cf81a55719e9359a6f",
+    "url": "/Elektron313/social-network/static/css/main.122b097f.chunk.css"
+  },
+  {
+    "revision": "8fe7c20a20461d3f540f",
+    "url": "/Elektron313/social-network/static/js/2.a296a163.chunk.js"
+  },
+  {
+    "revision": "e96da2ccb0da048072af",
+    "url": "/Elektron313/social-network/static/js/3.215abb44.chunk.js"
+  },
+  {
+    "revision": "98e7c67ab7d8c3a25725",
+    "url": "/Elektron313/social-network/static/js/4.08c0369d.chunk.js"
+  },
+  {
+    "revision": "f3cf81a55719e9359a6f",
+    "url": "/Elektron313/social-network/static/js/main.21d3fdee.chunk.js"
+  },
+  {
+    "revision": "f0d39e8c929b50a0aa28",
+    "url": "/Elektron313/social-network/static/js/runtime~main.a654c57b.js"
+  },
+  {
+    "revision": "6c3daf6085e46eea1698990ad3578119",
+    "url": "/Elektron313/social-network/static/media/preloader.6c3daf60.svg"
+  },
+  {
+    "revision": "8b1dbb1f4e4468b9cc31ced9c63fcfe7",
+    "url": "/Elektron313/social-network/static/media/user.8b1dbb1f.png"
+  }
+]);
